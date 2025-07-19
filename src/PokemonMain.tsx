212f92023e4ca33698d31147497e2579cd4ddbaf
asanmigu_LinkedIn <asanmiguel@linkedin.com>
2025-07-18 20:14:11 -0700

    fixed check pokemon api call

--- src/PokemonMain.tsx
+++ src/PokemonMain.tsx
@@ -51,7 +51,7 @@ const PokemonMain: React.FC = () => {
         setReveal(true);
         setTimeout(() => setPikachuImage("/pikachu.png"), 1200);
         try {
-            const response = await axios.get(`https://https://whos-that-pokemon-be.onrender.com/api/pokemon/checkRandomPokemon/${pokemonName}`);
+            const response = await axios.get(`https://whos-that-pokemon-be.onrender.com/api/pokemon/checkRandomPokemon/${pokemonName}`);
             if (response.data.correct === true) {
                 if (!hasGuessedCorrectly) {
                     setMessage("You Got It!");
